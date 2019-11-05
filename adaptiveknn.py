d7353ef538c1244cf863e6b7760114339c1e0d8a
seth <sethbarrash@gmail.com>
2019-11-05 17:54:07 -0600

    Reorganize adaptiveknn.py to put all spincom code together

--- adaptiveknn.py
+++ adaptiveknn.py
@@ -108,38 +108,6 @@ def generalizek_spincom(X, y, m, r, h, sigma):
     return g
 
 
-def generalizek_adaknn(X, y, alpha, kmax):
-    ktrain = np.zeros_like(y)
-    for i in range(len(X)):
-        ktrain[i] = fitk_adaknn(X, y, i, alpha, kmax)
-
-    X = atleast_2d_T(X)
-    mlp = MLPRegressor([15])
-    mlp.fit(X, ktrain)
-
-    return mlp
-
-
-def choose_k_from_random_subset(Krand, ysorted, y0):
-    error_min = np.Inf
-    for k in Krand:
-        error = np.mean(ysorted[:k]) - y0
-        if error < error_min:
-            error_min = error
-            kbest = k
-
-    return kbest
-
-
-def fitk_adaknn(X, y, idx, alpha, kmax):
-    alpha, kmax = int(alpha), int(kmax)
-
-    y0 = y[idx]
-    Xsorted, ysorted = get_neighborhood(X, y, idx)
-    Krand = np.random.choice(np.arange(kmax), alpha)
-    kbest = choose_k_from_random_subset(Krand, ysorted, y0)
-    return kbest
-
 
 def chisel_k(ki, n):
     k = int(ki)
@@ -155,30 +123,6 @@ def atleast_2d_T(X):
         else : return X
     except(AttributeError):
         return np.atleast_2d(X)
-
-
-###############################################################################
-## Train/test
-###############################################################################
-
-def knnTrainTest(X, y, Xtest, ytest, ki):
-    ytest = ytest.squeeze()
-    knr  = KNeighborsRegressor(n_neighbors = ki, algorithm = "brute")
-    t0 = datetime.now()
-    knr.fit(X, y)
-    t1 = datetime.now()
-    ## If this receives multiple one-dimensional testing data, it will 
-    ## misinterpret them as a single, multi-dimensional datum
-    yhat = knr.predict( np.atleast_2d(Xtest) )
-    errors = yhat.squeeze() - ytest
-    t2 = datetime.now()
-    normalized_errors = errors / np.sum(ytest ** 2)
-
-    trainTime = (t1 - t0).total_seconds()
-    testTime  = (t2 - t1).total_seconds()
-
-    return errors, normalized_errors, trainTime, testTime
-
 
 
 def spincomTrainTest(X, y, Xtest, ytest, hyperparams):
@@ -213,6 +157,126 @@ def spincomTrainTest(X, y, Xtest, ytest, hyperparams):
     testTime  = (t2 - t1).total_seconds()
 
     return errors, normalized_errors, ktest, trainTime, testTime
+
+
+def optimize_sigma(Xtrain, ytrain, Xvalid, yvalid, sigmas, m, r, h):
+    spincom_validation = pd.DataFrame({"error" : np.nan}, sigmas)
+    for sigma in sigmas:
+        hyperparams = {
+            "m"     : m,
+            "r"     : r,
+            "h"     : h,
+            "sigma" : sigma,
+        }
+        errors = spincomTrainTest(Xtrain, ytrain, Xvalid, yvalid, hyperparams)[0]
+        spincom_validation["error"][sigma] = np.mean( errors ** 2 )
+    sigmabest = spincom_validation["error"].idxmin()
+
+    return sigmabest, spincom_validation
+
+
+def real_data_trial_spincom(data_partition, sigma, m, r, h):
+    dataset, Xtrain, Xvalid, Xtest, ytrain, yvalid, ytest = data_partition
+    X = np.vstack((Xtrain, Xvalid))
+    y = np.append(ytrain, yvalid)
+
+    hyperparams = {"m" : m, "r" : r, "h" : h, "sigma" : sigma}
+    errors, normalized_errors, ktest, trainTime, testTime = spincomTrainTest(X, y, Xtest, ytest, hyperparams)
+    error = np.mean(errors ** 2)
+    normalized_error = np.mean(normalized_errors ** 2)
+
+    new_row = pd.Series({
+        "dataset" : dataset,
+        "sigma"   : sigma,
+        "m"       : m,
+        "r"       : r,
+        "h"       : h,
+        "mse"     : error,
+        "nmse"    : normalized_error,
+        "trainTime" : trainTime,
+        "testTime"  : testTime,
+    })
+    return new_row
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def fitk_adaknn(X, y, idx, alpha, kmax):
+    alpha, kmax = int(alpha), int(kmax)
+
+    y0 = y[idx]
+    Xsorted, ysorted = get_neighborhood(X, y, idx)
+    Krand = np.random.choice(np.arange(kmax), alpha)
+    kbest = choose_k_from_random_subset(Krand, ysorted, y0)
+    return kbest
+
+
+def generalizek_adaknn(X, y, alpha, kmax):
+    ktrain = np.zeros_like(y)
+    for i in range(len(X)):
+        ktrain[i] = fitk_adaknn(X, y, i, alpha, kmax)
+
+    X = atleast_2d_T(X)
+    mlp = MLPRegressor([15])
+    mlp.fit(X, ktrain)
+
+    return mlp
+
+
+def choose_k_from_random_subset(Krand, ysorted, y0):
+    error_min = np.Inf
+    for k in Krand:
+        error = np.mean(ysorted[:k]) - y0
+        if error < error_min:
+            error_min = error
+            kbest = k
+
+    return kbest
+
+
+
+
+###############################################################################
+## Train/test
+###############################################################################
+
+def knnTrainTest(X, y, Xtest, ytest, ki):
+    ytest = ytest.squeeze()
+    knr  = KNeighborsRegressor(n_neighbors = ki, algorithm = "brute")
+    t0 = datetime.now()
+    knr.fit(X, y)
+    t1 = datetime.now()
+    ## If this receives multiple one-dimensional testing data, it will 
+    ## misinterpret them as a single, multi-dimensional datum
+    yhat = knr.predict( np.atleast_2d(Xtest) )
+    errors = yhat.squeeze() - ytest
+    t2 = datetime.now()
+    normalized_errors = errors / np.sum(ytest ** 2)
+
+    trainTime = (t1 - t0).total_seconds()
+    testTime  = (t2 - t1).total_seconds()
+
+    return errors, normalized_errors, trainTime, testTime
+
 
 
 
@@ -310,22 +374,6 @@ def optimize_k(Xtrain, ytrain, Xvalid, yvalid, ks):
 
 
 
-def optimize_sigma(Xtrain, ytrain, Xvalid, yvalid, sigmas, m, r, h):
-    spincom_validation = pd.DataFrame({"error" : np.nan}, sigmas)
-    for sigma in sigmas:
-        hyperparams = {
-            "m"     : m,
-            "r"     : r,
-            "h"     : h,
-            "sigma" : sigma,
-        }
-        errors = spincomTrainTest(Xtrain, ytrain, Xvalid, yvalid, hyperparams)[0]
-        spincom_validation["error"][sigma] = np.mean( errors ** 2 )
-    sigmabest = spincom_validation["error"].idxmin()
-
-    return sigmabest, spincom_validation
-
-
 
 def alphas_kmaxs_to_index(alphas, kmaxs):
     n_alphas = len(alphas)
@@ -399,29 +447,6 @@ def real_data_trial_knn(data_partition, k):
     })
     return new_row
 
-
-def real_data_trial_spincom(data_partition, sigma, m, r, h):
-    dataset, Xtrain, Xvalid, Xtest, ytrain, yvalid, ytest = data_partition
-    X = np.vstack((Xtrain, Xvalid))
-    y = np.append(ytrain, yvalid)
-
-    hyperparams = {"m" : m, "r" : r, "h" : h, "sigma" : sigma}
-    errors, normalized_errors, ktest, trainTime, testTime = spincomTrainTest(X, y, Xtest, ytest, hyperparams)
-    error = np.mean(errors ** 2)
-    normalized_error = np.mean(normalized_errors ** 2)
-
-    new_row = pd.Series({
-        "dataset" : dataset,
-        "sigma"   : sigma,
-        "m"       : m,
-        "r"       : r,
-        "h"       : h,
-        "mse"     : error,
-        "nmse"    : normalized_error,
-        "trainTime" : trainTime,
-        "testTime"  : testTime,
-    })
-    return new_row
 
 def real_data_trial_adaknn(data_partition, alpha, kmax):
     dataset, Xtrain, Xvalid, Xtest, ytrain, yvalid, ytest = data_partition
